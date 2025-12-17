3d1fd9027bfd71df704ab2620ddc634ab62307a9
Pham Minh Khuong <phamminhkhuong1902@gmail.com>
2025-12-17 13:55:00 +0700

    them

--- Buoi17/bai1.php
+++ Buoi17/bai1.php
@@ -7,6 +7,11 @@
             echo "Thông tin học sinh <br>";
             echo "Tên học sinh:".$this->ten."<br>";
             echo "Tuổi học sinh:".$this->tuoi."<br>";
+            echo "Điểm học sinh:".$this->ten."<hr>";
+        }
+        function ThongtinHS1(){
+            echo "Thông tin học sinh <br>";
+            echo "Tên học sinh:".$this->ten."<br>";
             echo "Điểm học sinh:".$this->ten;
         }
     }
@@ -15,4 +20,6 @@
     $khuong->tuoi="21";
     $khuong->diem="9.0";
     $khuong->ThongtinHS();
+    $khuong->ThongtinHS1();
+
 ?>
